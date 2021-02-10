ca7825dcc31b1b9f140106f268ea52a7ee3b14e1
ccastill <Carlosc90@gmail.com>
2021-02-10 01:32:03 +0100

    Javadoc añadido

--- Refactorización/src/CCuenta.java
+++ Refactorización/src/CCuenta.java
@@ -1,66 +1,123 @@
+
+ /**@author carlos castillo fernandez
+    @version  1.0
+ */   
+
+/**Clase principal*/
 public class CCuenta {
 
-
+	/**MÈtodo getter del atributo Nombre
+	 * 
+	 * @return nombre
+	 */
     public String getNombre() {
 		return nombre;
 	}
 
+    /**MÈtodo setter del atributo Nombre
+	 * 
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**MÈtodo getter del atributo cuenta
+	 * 
+	 * @return cuenta
+	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
+	 /**MÈtodo setter del atributo cuenta
+		 * 
+		 * @param cuenta
+		 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**MÈtodo getter del atributo saldo
+	 * 
+	 * @return saldo
+	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
+	 /**MÈtodo setter del atributo saldo
+		 * 
+		 * @param saldo
+		 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**MÈtodo getter del atributo tipoInter√©s
+	 * 
+	 * @return tipoInter√©s
+	 */
 	public double getTipoInter√() {
 		return tipoInter√;
 	}
 
+	 /**MÈtodo setter del atributo tipoInter√
+		 * 
+		 * @param tipoInter√
+		 */
 	public void setTipoInter√(double tipoInter√) {
 		this.tipoInter√ = tipoInter√;
 	}
 
+	/**Variables encapsuladas*/
 	private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInter√©s;
 
+    /**Constructor b·sico*/
     public CCuenta()
     {
     }
-
+    
+    /**Constructor con par·metros por consola*/
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+    /**MÈtodo getter de saldo 
+     * 
+     * @return saldo
+     * */
     public double estado()
     {
         return saldo;
     }
 
+    /**MÈtodo setter de ingresar
+     * 
+     * 
+     * @param ingresar
+     * @exception En caso de que la cantidad a ingresar sea inferior a 0 devuelve un mensaje.
+     * En caso contrario lanza actualiza el saldo con la cantidad ingresada.
+     * */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
-
+    
+    /**MÈtodo setter de retirar 
+     * 
+     * @param saldo
+     * @exception En caso de que la cantidad a retirar sea inferior a 0 devuelve un mensaje.
+     * O si la cantidad es menor al saldo tambiÈn lanza una excepciÛn
+     * En caso contrario lanza actualiza el saldo con la cantidad ingresada.
+     * */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
--- Refactorización/src/Main.java
+++ Refactorización/src/Main.java
@@ -1,17 +1,27 @@
-public class Main {
 
+ /**@author carlos castillo fernandez
+    @version  1.0
+ */   
+public class Main {
+	
+	/**Clase principal*/
     public static void main(String[] args) {
         operativa_cuenta();
     }
-
+    
+    /**método operativa_cuenta
+     * @return texto informativo acerca del proceso de gestión de la cuenta.
+     *  */
 	public static void operativa_cuenta(float cantidad) {
 		CCuenta cuenta1;
         double saldoActual;
-
+        
+        /**Se instancia una calse ccuenta*/
         cuenta1 = new CCuenta("Antonio LÃ³pez","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
+        /**Se llevan a cabo unas excepciones en para los posibles errores.*/
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
